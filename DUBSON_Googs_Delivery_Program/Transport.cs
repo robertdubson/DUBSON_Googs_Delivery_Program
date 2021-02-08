ea9bfdeb1d7d25f8c81353f2ff2c48f706e7d994
robertdubson <dubson2001@gmail.com>
2021-02-08 22:29:48 +0200

    New Class for Transport

---
+++ DUBSON_Googs_Delivery_Program/Transport.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DUBSON_Goods_Delivery_Program
+{
+    public class Transport
+    {
+        // поле, що визначає швидкість виду транспорту field
+        private long _speed;
+
+
+        public long Speed {
+
+            get => _speed;
+        }
+        
+        // поле, що визначає, чи в дорозі зараз транспортний засіб
+        private bool _isCurrentlyBusy;
+
+        public bool isCurrentlyBusy {
+
+            get => _isCurrentlyBusy;
+            set => _isCurrentlyBusy = value;
+        
+        }
+
+        // конструктор
+        public Transport(Transport_Category category) {
+
+            // в залежності від категорії транспорту визначаємо швидкість, з якою засіб буде рухатись між містами
+
+            if (category == Transport_Category.Car) {
+                this._speed = 70;
+
+            }
+            else if (category == Transport_Category.Truck) {
+                this._speed = 60;
+
+            }
+            else if (category == Transport_Category.Giant_Truck) {
+
+                this._speed = 50;
+            
+            }
+            
+            this._isCurrentlyBusy = false;
+        }
+
+
+
+    }
+    // перераховуємо можливі види транспорту
+        public enum Transport_Category { 
+        Truck,
+        Giant_Truck,
+        Car,
+
+    }
+}
