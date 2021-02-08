4c58078acc4d6c458aa8e79b9361bd078e91f9e9
robertdubson <dubson2001@gmail.com>
2021-02-08 22:56:13 +0200

    Class for destination point

---
+++ DUBSON_Googs_Delivery_Program/Destination.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DUBSON_Goods_Delivery_Program
+{
+    public class Destination
+    {
+        //поле, що визначає відстань від пункту призначення до головного магазина
+        private long _distance;
+
+        public long Distance {
+
+            get => _distance;
+        
+        }
+
+        //поле, що визначає назву населеного пункту
+        private string _name;
+
+        public string Name {
+
+            get => _name;
+        }
+
+        // конструктор
+        public Destination(long distance, string name) {
+
+            _distance = distance;
+            _name = name;
+        
+        }
+    }
+}
