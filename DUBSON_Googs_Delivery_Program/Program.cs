e4d5412057a6ff1a4c290f7dd9beb76cdeaeed9e
robertdubson <dubson2001@gmail.com>
2021-02-08 21:16:32 +0200

    Add project files.

---
+++ DUBSON_Googs_Delivery_Program/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DUBSON_Goods_Delivery_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
