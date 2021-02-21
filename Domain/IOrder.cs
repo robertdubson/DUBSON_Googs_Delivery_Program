3a619cf9d2b248006e7c07319506e5698e3f17c7
Robert Dubson <dubson2001@gmail.com>
2021-02-21 17:07:41 +0200

    Intarfaces for representation of entities in Domain (intarfacec for classes of the model).

---
+++ Domain/IOrder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public interface IOrder
+    {
+        IDestination Destination { get; set; }
+
+        IProduct Product{ get; set; }
+
+        DateTime TimeOfOrdering { get; set; }
+
+        double TimeNeededForDelivery { get; set; }
+
+
+    }
+}
---
+++ Domain/IProduct.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public interface IProduct
+    {
+        string Name { get; set; }
+        
+        double Weight { get; set; }
+
+        double Volume { get; set; }
+
+        double Price { get; set; }
+
+        double TimeForPreparation { get; set; }
+
+    }
+}
